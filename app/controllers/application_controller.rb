c94ca50e88a72415ee2f8a63421c4615e5991c2c
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-11-11 14:40:22 +0000

    [Update]Signin,Signyp後遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    books_path
+    user_path(current_user)
   end
-  
+
   def after_sign_up_path_for(resource)
-    books_path
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
