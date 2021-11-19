d4794b80850c78463e30423e4408a78f74fdee12
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-11-19 06:23:43 +0000

    [Fix]from

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -13,7 +13,7 @@ class FavoritesController < ApplicationController
     @favorite = current_user.favorites.find_by(book_id: @book.id)
     @favorite.destroy
     to = Time.current.at_end_of_day
-    from = (to - 2.week).at_beginning_of_day
+    from = (to - 1.week).at_beginning_of_day
     @books = Book.includes(:favorited_users).where(created_at: from...to).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
   end
 
